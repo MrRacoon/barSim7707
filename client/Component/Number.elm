84171d41beb916e2a0706260eabae914a7dee267
Erik Sutherland <erik.misu@gmail.com>
2017-01-18 23:12:07 -0800

    add timers and style numbers by member

--- client/Component/Number.elm
+++ client/Component/Number.elm
@@ -2,28 +2,26 @@ module Component.Number exposing (render)
 
 import Html exposing (Html, li, text)
 import Html.Attributes exposing (style)
+import List exposing (member)
 
 
 defaultColor : String
 defaultColor = "#000000"
 
 pickedColor : String
-pickedColor = "#AF32A1"
+pickedColor = "#00FF00"
 
-numberColor : Maybe Int -> Int -> String
-numberColor lastDrawn cur =
-  case lastDrawn of
-    Nothing -> defaultColor
-    Just x  ->
-      if cur == x
-        then pickedColor
-        else defaultColor
+numberColor : List Int -> Int -> String
+numberColor picked cur =
+  if member cur picked
+    then pickedColor
+    else defaultColor
 
 render : List Int -> Maybe Int -> Int -> Html a
 render picked lastDrawn cur =
   li
     [ style
-      [("color", numberColor lastDrawn cur)
+      [("color", numberColor picked cur)
       ]
     ]
     [ text (toString cur) ]
--- client/Main.elm
+++ client/Main.elm
@@ -1,22 +1,28 @@
 module Main exposing (..)
 
--- import Debug exposing (log)
-import Html exposing (..)
-import Html.Events exposing (onClick)
-import List exposing (map, range)
-import Component.Grid
 import Platform.Cmd as Cmd
 import Platform.Sub as Sub
+
+import Html exposing (Html, program, div, span, button, text)
+import Html.Events exposing (onClick)
+import List exposing (map, range, member, length)
+import Random exposing (generate, int)
+import Time exposing (Time, second, every)
+
+import Component.Grid
 
 type Message
   = Reset
   | GetNumber
   | NewNumber Int
+  | TimerTick Time
 
 type alias Model =
   { avail     : List Int
   , picked    : List Int
   , lastDrawn : Maybe Int
+  , startTime : Time
+  , curTime   : Time
   }
 
 model : Model
@@ -24,19 +30,28 @@ model =
   { avail  = range 1 20
   , picked = []
   , lastDrawn = Nothing
+  , startTime = 0
+  , curTime = 0
   }
 
 init = (model, Cmd.none)
 
 update : Message -> Model -> (Model, Cmd Message)
 update msg model =
-  case msg of
+  let newNumber = generate NewNumber (int 1 20)
+  in case msg of
     Reset ->
       ({ model | picked = [], lastDrawn = Nothing }, Cmd.none)
     GetNumber ->
-      update (NewNumber 4) model
+      if length model.avail == length model.picked
+        then ({ model | picked = [], lastDrawn = Nothing }, Cmd.none)
+        else (model, newNumber)
     NewNumber x ->
-      ({ model | picked = model.picked ++ [x], lastDrawn = Just 4 }, Cmd.none)
+      if member x model.picked
+        then (model, newNumber)
+        else ({ model | picked = model.picked ++ [x], lastDrawn = Just x }, Cmd.none)
+    TimerTick t ->
+      ({ model | curTime = t }, newNumber)
 
 view : Model -> Html Message
 view model =
@@ -45,8 +60,8 @@ view model =
     [ Component.Grid.render model.avail model.picked model.lastDrawn
     , button [onClick GetNumber] [text "NewNum"]
     , button [onClick Reset] [text "Reset"]
-    , div
-      []
+    , span [] [ text <| toString model.startTime ++ toString model.curTime]
+    , div []
       [ case model.lastDrawn of
         Nothing ->
           text (toString model.lastDrawn)
@@ -55,7 +70,12 @@ view model =
       ]
     ]
 
-subscriptions model = Sub.none
+subscriptions model =
+  if length model.avail == length model.picked
+    then Sub.none
+    else Sub.batch
+      [ every second TimerTick
+      ]
 
 main = program
   { init          = init
