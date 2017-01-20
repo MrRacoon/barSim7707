0565ccf51950c98481e7da7affd414474cb6e8e6
Erik Sutherland <erik.misu@gmail.com>
2017-01-19 20:30:02 -0800

    try to use date to tweak the timer

--- client/Component/Timer.elm
+++ client/Component/Timer.elm
@@ -2,7 +2,11 @@ module Component.Timer exposing (render)
 
 import Html exposing (Html, span, text)
 import Html.Attributes exposing (style)
-import Time exposing (..)
+import Time exposing (inMinutes, inSeconds)
+import Date exposing (fromTime, hour, second)
+import String exposing (append)
+
+
 
 render start cur =
   let attrs = [ style [("float", "right")] ]
@@ -10,4 +14,6 @@ render start cur =
     Nothing -> span attrs [ text "Timer: ---" ]
     Just s  ->
       let diff = cur - s
-      in span attrs [ text <| toString <| inSeconds diff ]
+          min  = hour <| fromTime diff
+          sec  = second <| fromTime diff
+      in span attrs [ text <| ("Timer: " ++ (toString min) ++ ":" ++ (toString sec)) ]
