b4af8ec55df37d8b2d4fa41cf19386fdf17c7491
Erik Sutherland <erik.misu@gmail.com>
2017-08-05 17:13:27 -0700

    lint and condense server

--- server/src/RNG.hs
+++ server/src/RNG.hs
@@ -4,7 +4,7 @@ import System.Random
 import Data.List (nub)
 
 randomSample :: (Int, Int) -> Int -> IO [Int]
-randomSample range n = newStdGen >>= return . take n . nub . randomRs range
+randomSample range n = fmap (take n . nub . randomRs range) newStdGen
 
 kenoSample :: IO [Int]
 kenoSample = randomSample (1, 80) 20
